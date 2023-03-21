6835de90c0468dae3e384774e53485134cd29344
kweeka <mtalbi29@yahoo.com>
2023-03-21 09:23:17 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_092017) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_093941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,15 +19,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_092017) do
     t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_expenses_on_author_id"
   end
 
   create_table "expenses_groups", id: false, force: :cascade do |t|
     t.bigint "expense_id", null: false
     t.bigint "group_id", null: false
-    t.index %w[expense_id group_id], name: "index_expenses_groups_on_expense_id_and_group_id"
-    t.index %w[group_id expense_id], name: "index_expenses_groups_on_group_id_and_expense_id"
+    t.index ["expense_id", "group_id"], name: "index_expenses_groups_on_expense_id_and_group_id"
+    t.index ["group_id", "expense_id"], name: "index_expenses_groups_on_group_id_and_expense_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -35,7 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_092017) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
+    t.bigint "author_id"
     t.index ["author_id"], name: "index_groups_on_author_id"
   end
 
@@ -43,6 +43,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_092017) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "expenses", "users", column: "author_id"
