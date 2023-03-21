27bbb20a7ca3c846c94e4851aaea7496e0c57e22
Kweeka <mtalbi29@yahoo.com>
2023-03-21 15:29:19 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,12 @@ Rails.application.routes.draw do
   get 'categories', to: "category#index", as: "categories"
   get 'categories/new', to: "category#new", as: "new_category"
   post 'categories/create', to: "category#create", as: "create_category"
+  delete 'categories/:category_id/destroy', to: "category#destroy", as: 'destroy_category'
 
   # Expenses
   get 'categories/:category_id/expenses', to: "expenses#index", as: 'expenses'
+  get 'categories/:category_id/expenses/new', to: "expenses#new", as: 'new_expense'
   get 'categories/:category_id/expenses/:expense_id', to: "expenses#show", as: 'expense'
+  post 'categories/:category_id/expenses/create', to: "expenses#create", as: 'create_expense'
+  post 'categories/:category_id/expenses/:expense_id/destroy', to: "expenses#destroy", as: 'destroy_expense'
 end
