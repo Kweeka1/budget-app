109d6d090567f7864ec1392b1c94f9b3f031f430
Kweeka1 <mtalbi29@yahoo.com>
2023-03-21 12:15:17 +0300

    Add index method

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,6 +2,8 @@ class ExpensesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @expenses_groups = Expenses.includes(:groups).where(groups: { id: params[:category_id] })
+    puts @expenses_groups
   end
 
   def show
