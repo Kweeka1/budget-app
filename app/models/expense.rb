c8c01da180a1d37b41a5e53b19169329d7bfa7aa
kweeka <mtalbi29@yahoo.com>
2023-03-21 09:21:16 +0100

    Add relations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,4 @@
 class Expense < ApplicationRecord
+  belongs_to :user, foreign_key: :author_id
+  has_many :groups
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  belongs_to :user, foreign_key: :author_id
+  has_many :expenses
 end
