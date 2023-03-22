d9dda8db9c14ae397d458f5c123405e6994fec0f
kweeka <mtalbi29@yahoo.com>
2023-03-22 13:29:05 +0100

    Add model specs

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'Group' do
+
+  let(:user1) { User.first }
+
+  before do
+    subject.save
+  end
+
+  subject do
+    if user1.nil?
+      user1 = User.create(name: 'Peter', email: 'peter@gmail.com', encrypted_password: '123456')
+    end
+    Group.new(name: 'School', icon: 'https://www.imgur.com/dA4D8aQ', author_id: user1.id)
+  end
+
+  context 'Model should be valid' do
+    it 'Name should be present' do
+      subject.name = nil
+      expect(subject.valid?).to be false
+    end
+
+    it 'Icon should be present' do
+      subject.icon = nil
+      expect(subject.valid?).to be false
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.new(name: 'Peter', email: 'peter@gmail.com', encrypted_password: '123456')
+  end
+
+  before { subject.save }
+
+  context 'Model should be valid' do
+    it 'Name should be present' do
+      subject.name = nil
+      expect(subject.valid?).to be false
+    end
+
+    it 'Email should be present' do
+      subject.email = nil
+      expect(subject.valid?).to be false
+    end
+
+    it 'Password should be present' do
+      subject.encrypted_password = nil
+      expect(subject.valid?).to be false
+    end
+  end
 end
